5195d3c3423bd80736eaa72e0a41138a45cac3d5
上島春輝 <h.ueji.43@gmail.com>
2022-05-08 14:44:20 +0900

    オブジェクト指向の基本

---
+++ src/class.ts
@@ -0,0 +1,20 @@
+class Person {
+
+  name: string;
+  private age: number;
+
+  constructor(initName: string, initAge: number) {
+    this.name = initName;
+    this.age = initAge;
+  }
+  incrementAge() {
+    this.age += 1;
+  }
+  greeting(this: Person) {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+  }
+}
+let person2: Person;
+const quill = new Person('Quill', 38);
+quill.incrementAge();
+quill.greeting();
