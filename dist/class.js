e3c42740233e04694f97921d7cd847d01b3b088e
上島春輝 <h.ueji.43@gmail.com>
2022-05-08 15:13:07 +0900

    継承

--- dist/class.js
+++ dist/class.js
@@ -11,7 +11,28 @@ class Person {
         console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     }
 }
-let person2;
-const quill = new Person('Quill', 38);
-quill.incrementAge();
-quill.greeting();
+class Teacher extends Person {
+    constructor(name, age, _subject) {
+        super(name, age);
+        this._subject = _subject;
+    }
+    get subject() {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error('There is no subject.');
+        }
+        this._subject = value;
+    }
+    greeting() {
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+    }
+}
+const teacher = new Teacher('Quill', 38, 'Math');
+teacher.subject = 'Music';
+console.log(teacher.subject);
+teacher.greeting();
--- src/class.ts
+++ src/class.ts
@@ -1,6 +1,6 @@
 class Person {
 
-  constructor(public name: string, private age: number) {
+  constructor(readonly name: string, protected age: number) {
   }
   incrementAge() {
     this.age += 1;
@@ -9,7 +9,28 @@ class Person {
     console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
   }
 }
-let person2: Person;
-const quill = new Person('Quill', 38);
-quill.incrementAge();
-quill.greeting();
+
+class Teacher extends Person {
+  get subject() {
+    if (!this._subject) {
+      throw new Error('There is no subject.');
+    }
+    return this._subject
+  }
+  set subject(value) {
+    if (!value) {
+      throw new Error('There is no subject.');
+    }
+    this._subject = value;
+  }
+  constructor(name: string, age: number, private _subject: string) {
+    super(name, age);
+  }
+  greeting() {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+  }
+}
+const teacher = new Teacher('Quill', 38, 'Math');
+teacher.subject = 'Music';
+console.log(teacher.subject);
+teacher.greeting();
